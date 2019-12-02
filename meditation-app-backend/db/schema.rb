398ebf8bbc2ce3870562f0de867ba0df869746e1
geewey <geewey@gmail.com>
2019-12-02 14:34:00 +0000

    Generated, migrated, and seeded back-end

---
+++ meditation-app-backend/db/schema.rb
@@ -0,0 +1,39 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_12_02_140921) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "track_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["track_id"], name: "index_favorites_on_track_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "tracks", force: :cascade do |t|
+    t.string "title"
+    t.integer "length"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "favorites", "tracks"
+  add_foreign_key "favorites", "users"
+end
--- meditation-app-backend/db/seeds.rb
+++ meditation-app-backend/db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+require 'pry'
+
+Favorite.delete_all
+# Track.delete_all
+# User.delete_all
+
+# users_name = [
+#     'Natalie',
+#     'Prince',
+#     'Dick',
+#     'Rachel',
+#     'Garry',
+#     'Jason',
+#     'Matt',
+#     'Niky',
+#     'Ashley'
+#   ]
+   
+
+# users_name.each do |name|
+#     User.create(username: name)
+# end
+
+# 30.times {
+#    Track.create(title: Faker::Quote.unique.yoda, length: rand(2..30))
+# }
+
+10.times {
+    Favorite.create(user_id: User.all.sample.id, track_id: Track.all.sample.id)
+}
+
