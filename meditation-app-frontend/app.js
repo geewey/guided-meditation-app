333eb515bd22751e0a51678bdb9bbb4034a82d69
skjwise <skjwise@hotmail.com>
2019-12-03 11:15:19 +0000

    getting tracks in index.js not working

--- meditation-app-frontend/app.js
+++ meditation-app-frontend/app.js
@@ -1,13 +1,58 @@
+const app = () => {
 // CONSTs
 const trackPicker = document.querySelector('.track-picker');
 const trackPlayerPanel = document.querySelector('.track-player-panel');
+let http = import('http');
+let url = import('url');
+let fs = import('fs');
+const arr = [];
+let bufferString;
+
+const csvHandler = (req, res) => { 
+    fs.readFile('./tracks/Tracklist.csv', function(err, data){
+        if (err){
+            return console.log(err);
+        }
+        bufferString = data.toString();
+
+        arr = bufferString.split('\n');
+        console.log(arr);
+
+        let jsonObject = [];
+        let headers = arr[0].split(',');
+        for(let i = 1; i < arr.length; i++){
+            let data = arr[i].split(',')
+            let obj = {};
+            for(let j = 0; j < data.length; j++){
+                obj[headers[j].trim()] = data[j].trim();
+            }
+            jsonObject.push(obj);
+        }
+        JSON.stringify(jsonObject);
+
+        // for(i = 0; i < arr.length; i++){
+        //     JSON.stringify(arr[i]);
+        // }
+        // JSON.parse(arr)
+        // res.send(arr)
+    })
+}
+
+
+
+
 
 //FUNCTIONs
 const init = () => {
-    trackCards();
+    renderTracks();
+    csvHandler();
 }
 
-const trackCards = () => {
+const renderTracks = () => {
+    fs.forEach(csv => trackCards(csv))
+}
+
+const trackCards = (csv) => {
     //at some point display the track cards with images.
     const card = document.createElement('div')
     card.className = "card"
@@ -39,4 +84,8 @@ init();
 // In the trackPlayerPanel section we need to display the selected track details:
 // track title - track length - track timer - favorite button
 
-// 
+
+}
+
+
+app();
